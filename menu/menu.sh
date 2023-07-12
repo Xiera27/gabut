16958b0557a89f57c68c278490b5b9ff4fd88c63
Xiera27 <127057823+Xiera27@users.noreply.github.com>
2023-07-13 00:07:48 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#FN
-#RERECHAN02
-#indo-ssh.com
+#
+#
+#
 # =========================================
 # OS Uptime
 uptime="$(uptime -p | cut -d " " -f 2-10)"
@@ -9,42 +9,7 @@ DATE2=$(date -R | cut -d " " -f -5)
 tram=$(free -m | awk 'NR==2 {print $2}')
 uram=$(free -m | awk 'NR==2 {print $3}')
 fram=$(free -m | awk 'NR==2 {print $4}')
-#link izin ip vps
-url_izin='https://raw.githubusercontent.com/nilaa27/cinta/main/vps'
-
-# Mendapatkan IP VPS saat ini
-ip_vps=$(curl -s ifconfig.me)
-
-# Mendapatkan isi file izin.txt dari URL
-izin=$(curl -s "$url_izin")
-
-# Memeriksa apakah konten izin.txt berhasil didapatkan
-if [[ -n "$izin" ]]; then
-  while IFS= read -r line; do
-    # Memisahkan nama VPS, IP VPS, dan tanggal kadaluwarsa
-    nama=$(echo "$line" | awk '{print $1}')
-    ipvps=$(echo "$line" | awk '{print $2}')
-    tanggal=$(echo "$line" | awk '{print $3}')
-
-    # Memeriksa apakah IP VPS saat ini cocok dengan IP VPS yang ada di izin.txt
-    if [[ "$ipvps" == "$ip_vps" ]]; then
-      echo "Nama VPS: $nama"
-      echo "IP VPS: $ipvps"
-      echo "Tanggal Kadaluwarsa: $tanggal"
-      break
-    fi
-  done <<< "$izin"
-
-  # Memeriksa apakah IP VPS ditemukan dalam izin.txt
-  if [[ "$ipvps" != "$ip_vps" ]]; then
-    echo "IP VPS tidak ditemukan dalam izin.txt"
-    exit 0
-  fi
-else
-  echo "Konten izin.txt tidak berhasil didapatkan dari URL"
-  exit 0
-fi
-
+################
 
 vlx=$(grep -c -E "^#& " "/etc/xray/config.json")
 let vla=$vlx/2
