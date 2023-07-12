cf8d061032c267adecf3c6d590ec5e6fc261ae41
Xiera27 <127057823+Xiera27@users.noreply.github.com>
2023-07-12 23:29:15 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ domain=$(cat /etc/xray/domain)
 date=$(date +"%Y-%m-%d")
 
 #link izin ip vps
-url_izin='https://raw.githubusercontent.com/nilaa27/cinta/main/vps'
+url_izin="https://raw.githubusercontent.com/nilaa27/cinta/main/vps"
 
 # Mendapatkan IP VPS saat ini
 ip_vps=$(curl -s ifconfig.me)
